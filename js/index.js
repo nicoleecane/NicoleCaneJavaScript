503a3d529b5a28f1de4f13c066e97f51dbcb9e3d
NicoleCane <nicolee.c1993@gmail.com>
2023-08-09 03:41:13 +0800

    commit con ultimos detalles para la entrega

--- js/index.js
+++ js/index.js
@@ -8,10 +8,11 @@ let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 
 const getCervezas = async () => {
-  const response = await fetch("./js/data.json");
+  const response = await fetch("/data.json");
   const data = await response.json();
   console.log(data);
   createBeerCards(data);
+
 }
 
 getCervezas();
@@ -20,65 +21,86 @@ getCervezas();
 
 
 const divRow = document.createElement("div");
-divRow.classList.add("row", "w-100");
+  divRow.classList.add("row", "w-100");
+  
+  function createBeerCards(data) {
+    data.forEach(cerveza => {
 
-function createBeerCards(data) {
-  for (const cerveza of data) {
-    const divCard = document.createElement("div");
-    divCard.classList.add("card", "col-4");
-    divCard.innerHTML = `
-   <div class='card-body'>
-      <img src='${cerveza.imgUrl}' class='card-img-top' alt=''
-      <h3>Type: ${cerveza.tipo}</h3>
-      <h3>Name: ${cerveza.nombre}</h3>
-      <p>A%: ${cerveza.porcentajeAlcohol}</p>
-      <p>Description:  ${cerveza.descripcion}</p>
-      <p> Price: ${cerveza.precio }</p>
-      <div class='card-footer'>
-        <button id='addToCart-${
-          cerveza.id
-        }' class='btn btn-outline-dark' w-100 > Add to Cart</button>
-      </div>
-   </div>
-    `;
-    divRow.appendChild(divCard);
-    listadoCervezas.appendChild(divRow);
-  
-    const boton = document.getElementById(`addToCart-${cerveza.id}`);
-  
-    boton.addEventListener("click", () => {
-     
-      const repeat =carrito.some((repeatCerveza) => repeatCerveza.id === cerveza.id);
-     
-      if (repeat){
-        carrito.map((cerveza) =>{
-          if(cerveza.id === cerveza.id){
-            cerveza.cantidad++;
-          } 
-        })
-      }else{
-        carrito.push({
-        id:cerveza.id,
-        img:cerveza.imgUrl,
-        nombre:cerveza.nombre,
-        precio:cerveza.precio,
-        cantidad:cerveza.cantidad,
+      
+        const divCard = document.createElement("div");
+        divCard.classList.add("card", "col-4");
+        divCard.innerHTML = `
+       <div class='card-body'>
+          <img src='${cerveza.imgUrl}' class='card-img-top' alt=''
+          <h3>Type: ${cerveza.tipo}</h3>
+          <h3>Name: ${cerveza.nombre}</h3>
+          <p>A%: ${cerveza.porcentajeAlcohol}</p>
+          <p>Description:  ${cerveza.descripcion}</p>
+          <p> Price: ${cerveza.precio }</p>
+          <div class='card-footer'>
+            <button id='addToCart-${
+              cerveza.id
+            }' class='btn btn-outline-dark' w-100 > Add to Cart</button>
+          </div>
+       </div>
+        `;
+       
+        divRow.appendChild(divCard);
+        listadoCervezas.appendChild(divRow);
+      
+        const boton = document.getElementById(`addToCart-${cerveza.id}`);
+      
+        boton.addEventListener('click', () => {
+   
+          Toastify({
+              text: " La Cerveza fue agregada al Carrito!",
+              duration: 3000
+          }).showToast();
+      })
+      
+
+
+        boton.addEventListener("click", () => {
+
+          const repeat =carrito.some((repeatCerveza) => repeatCerveza.id === cerveza.id);
+         
+          if (repeat){
+            carrito.map((cerveza) =>{
+              if(cerveza.id === cerveza.id){
+                cerveza.cantidad++;
+              } 
+            })
+          }else{
+            carrito.push({
+            id:cerveza.id,
+            img:cerveza.imgUrl,
+            nombre:cerveza.nombre,
+            precio:cerveza.precio,
+            cantidad:cerveza.cantidad,
+          });
+        }
+        
+           
+
+         carritoCounter();
+         saveLocal();
+        });
+      
+      
+      
       });
-    }
-    
-     carritoCounter();
-     saveLocal();
-    });
+      
+    }; 
+  //createBeerCards();
   
-  
-  
-  };
-}
-createBeerCards();
+  const saveLocal = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito ));
+  }
 
-const saveLocal = () => {
-  localStorage.setItem("carrito", JSON.stringify(carrito ));
-}
+
+
+
+
 
 
 
